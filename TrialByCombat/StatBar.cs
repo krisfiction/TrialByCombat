00e5a39de5f997a58471db4d0496f332effe2b5d
kris fiction <krisfiction@gmail.com>
2021-12-16 09:28:43 -0500

    added Health Bars (not final but working)

--- TrialByCombat/StatBar.cs
+++ TrialByCombat/StatBar.cs
@@ -20,6 +20,15 @@ namespace TrialByCombat
             Console.WriteLine(("").PadRight(80, '*'));
             Console.WriteLine();
 
+
+            //testing for health bar - right now only displays health percentage
+            //should display as [********  ] as 80 percent health
+            //should display as [**********] as 100 percent health
+            //should display as [***       ] as 30 percent health
+            Console.WriteLine($"health bar --> {player.DisplayHealthBar()}%");
+            Console.WriteLine($"health bar --> {monster.DisplayHealthBar()}%");
+
+
             Console.ForegroundColor = ConsoleColor.White;
         }
     }
