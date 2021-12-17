f06844944c1d25e5d8c00b8e76914259a73671ba
kris fiction <krisfiction@gmail.com>
2021-12-17 09:28:35 -0500

    moved healthbar into statbar

--- TrialByCombat/StatBar.cs
+++ TrialByCombat/StatBar.cs
@@ -9,25 +9,15 @@ namespace TrialByCombat
             Console.ForegroundColor = ConsoleColor.Blue; // makes text blue - may be removed
 
             Console.WriteLine();
-            Console.WriteLine("********************************************************************************");
-            
+            Console.WriteLine(("").PadRight(80, '*'));
+
             //this needs work if it is going to be used
             //Console.WriteLine("{0,-10}{1,-4}{2,-5}{3,-15}{4,-5}{5,20}{6,5}", Player.Name, "HP:", Player.HP, "Magic Shield:", Player.Shield, "Armor Bonus:", (Player.ChestArmorBonus + Player.HeadArmorBonus));
-            
-            Console.WriteLine(player.Name + "\tHP " + player.Health + "/" + player.HealthMax + "\tMagic Shield: " + player.Shield + "\t\tArmor Bonus: " + player.AC);
-            Console.WriteLine(monster.Name + "\tHP " + monster.Health + "/" + monster.HealthMax);
-            //Console.WriteLine("********************************************************************************");
+
+            Console.WriteLine($"{player.Name} \tHP {player.DisplayHealthBar()} {player.Health}/{player.HealthMax} \tMagic Shield: {player.Shield} \tArmor Bonus: {player.AC}");
+            Console.WriteLine($"{monster.Name} \tHP {monster.DisplayHealthBar()} {monster.Health}/{monster.HealthMax}");
             Console.WriteLine(("").PadRight(80, '*'));
             Console.WriteLine();
-
-
-            //testing for health bar - right now only displays health percentage
-            //should display as [********  ] as 80 percent health
-            //should display as [**********] as 100 percent health
-            //should display as [***       ] as 30 percent health
-            Console.WriteLine($"health bar --> {player.DisplayHealthBar()}%");
-            Console.WriteLine($"health bar --> {monster.DisplayHealthBar()}%");
-
 
             Console.ForegroundColor = ConsoleColor.White;
         }
